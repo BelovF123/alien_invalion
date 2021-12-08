b45c16cf711d53be66a10136edb892d118df68ec
Belov <fdor.belov.2003@mail.ru>
2021-12-08 18:27:37 +0300

    ñîçäàí ôëîò áåç êîëëèçèé. ôëîò äâèæåòñÿ âåðòèêàëüíî âíèç ñ ôèêñèðîâàííîé ñêîðîñòüþ

--- alien_invasion.py
+++ alien_invasion.py
@@ -12,11 +12,14 @@ def run():
     bg_color = (230, 230, 230)
     ship = Ship(screen)
     bullets = Group()
+    aliens = Group()
+    control.create_army(screen, aliens)
 
     while True:
         control.events(screen, ship, bullets)
         ship.update_ship()
-        control.update(bg_color, screen, ship, bullets)
+        control.update(bg_color, screen, ship, aliens, bullets)
         control.update_bullets(bullets)
+        control.update_aliens(aliens)
 
 run()
--- control.py
+++ control.py
@@ -1,7 +1,7 @@
 import pygame
 import sys
 from bullet import Bullet
-
+from alien import Alien
 
 def events(screen, ship, bullets):
     """управление кораблём"""
@@ -23,11 +23,12 @@ def events(screen, ship, bullets):
             if event.key == pygame.K_a:
                 ship.mleft = False
 
-def update(bg_color, screen, ship, bullets):
+def update(bg_color, screen, ship, aliens, bullets):
     screen.fill(bg_color)
     for bullet in bullets.sprites():
         bullet.draw_bullet()
     ship.output()
+    aliens.draw(screen)
     pygame.display.flip()
 
 def update_bullets(bullets):
@@ -35,3 +36,25 @@ def update_bullets(bullets):
     for bullet in bullets.copy():
         if bullet.rect.bottom < 0:
             bullets.remove(bullet)
+
+def update_aliens(aliens):
+    aliens.update()
+
+def create_army(screen, aliens):
+
+    alien = Alien(screen)
+    alien_width = alien.rect.width
+    alien_hight = alien.rect.height
+
+    number_alien_x = int((500 - 2 * alien_width) / alien_width)
+    number_alien_y = 3
+    for row_number in range(number_alien_y ):
+        for alien_number in range (number_alien_x):
+            alien = Alien(screen)
+            alien.x = alien_width + alien_width * alien_number
+            alien.y = alien_hight + alien_hight * row_number
+            alien.rect.x = alien.x
+            alien.rect.y = alien.rect.height + alien.rect.height * row_number
+            aliens.add(alien)
+
+
